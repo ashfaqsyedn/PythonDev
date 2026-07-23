70dbbf9116a2f1ea6f74c48263771c6adf015a6c
Syed Ashfaq N <ashfaqsyed.ai@gmail.com>
2026-07-23 23:20:42 +0530

    Add files via upload

---
+++ 1000 exercises/boolean_comparison.py
@@ -0,0 +1,35 @@
+ a = "42"
+ b = 42
+
+ print(a == b)
+ print(None == None)
+ print(None == False)
+
+ x = 12
+ y = 3
+
+ print( x > y)
+
+ x = "12"
+ y = "3"
+ print(x > y)
+
+ x = "12"
+ y = 3
+
+ print(x > y)
+
+ x = 12
+ y = "3"
+ print(x > y)
+
+
+"""
+if CoND:
+    do something
+
+else:
+    do something else
+"""
+
+
