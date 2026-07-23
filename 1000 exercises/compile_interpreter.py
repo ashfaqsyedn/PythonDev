9d421e508f1fa232b1c8950448474dcab993db56
Syed Ashfaq N <ashfaqsyed.ai@gmail.com>
2026-07-23 23:24:14 +0530

    Add files via upload

---
+++ 1000 exercises/compile_interpreter.py
@@ -0,0 +1,28 @@
+ x = 3
+ input(x)
+
+ if x > 3
+
+ a = 2
+ b = 3
+ c = 2.3
+
+ d = a+b
+
+ print(d)
+ print(a+b)
+ print(a+c)
+ print(b / a)
+ print(b // a)
+ print(a * c)
+ print(a ** b)
+
+ a += 7
+ print(a)
+
+
+ a += 1
+ print(a)
+ a -= 1
+ print(a)
+
